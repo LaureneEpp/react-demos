44ab6077f80a99443aff9657cbcfdad211b08aba
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2023-09-14 17:39:50 +0200

    created a weather pp: backend: rails & front-end: react

---
+++ 002-weather-rails-app/app/javascript/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Routes from "../routes/Index";
+import WeatherComponent from "../components/WeatherComponent"; 
+export default props => (
+  <>
+    {Routes}
+  </>
+);
