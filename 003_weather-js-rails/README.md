6d4bf4b6436cf44b4f8850dc20283e22350d4d31
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2023-09-18 17:54:57 +0200

    Integrate submodule content into subfolder

---
+++ 003_weather-js-rails/README.md
@@ -0,0 +1,3 @@
+My react laboratory
+
+The purpose of this repository is to present my small projects that I develop while trying to learn React. Learning by practising !
---
+++ 003_weather-js-rails/public/assets/weather-ae1122953e581d7ab0b45f9d46bf5bd1e6502c3ed417aec7bb4759dacc79861d.js
@@ -0,0 +1,90 @@
+(() => {
+  // app/javascript/weather.js
+  document.addEventListener("DOMContentLoaded", () => {
+    const cityInput = document.getElementById("city-input");
+    const weatherDisplay = document.getElementById("weather-display");
+    const errorDisplay = document.getElementById("error-display");
+    let weatherData;
+    document.getElementById("weather-form").addEventListener("submit", (event) => {
+      event.preventDefault();
+      const city = cityInput.value.trim();
+      if (!city) {
+        console.log("Test no city");
+      }
+      fetch(`/weather?city=${city}`).then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("Fail to fetch weather data!");
+        }
+      }).then((data) => {
+        weatherData = data;
+        displayWeather(weatherData);
+        errorDisplay.textContent = "";
+        appendInfoButton();
+      }).catch((error) => {
+        handleApiError(error.message);
+      });
+    });
+    function displayWeather(data) {
+      if (!data) {
+        errorDisplay.textContent = "Weather data is undefined.";
+        return;
+      }
+      const { name, sys, main, weather } = data;
+      const weatherHtml = `
+        <div class="card bg_blue-dark-color p-3 primary-color" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">
+              Weather in ${name}, ${sys.country}
+            </h5>
+            <p class="card-text"><span class="fw-semibold">Temperature:</span> ${main.temp}\xB0C</p>
+            <p class="card-text"><span class="fw-semibold">Conditions:</span> ${weather[0].description}</p>
+          </div>
+        </div>
+      `;
+      weatherDisplay.innerHTML = weatherHtml;
+      errorDisplay.textContent = "";
+    }
+    function handleApiError(errorMessage) {
+      errorDisplay.textContent = errorMessage;
+      weatherDisplay.textContent = "";
+    }
+    function appendInfoButton() {
+      const infoButton = document.createElement("button");
+      infoButton.textContent = "+";
+      infoButton.id = "info-button";
+      infoButton.classList.add("btn", "btn-light", "mt-3");
+      infoButton.addEventListener("click", () => {
+        displayMoreInfo(weatherData);
+        console.log("Test btn");
+      });
+      weatherDisplay.appendChild(infoButton);
+    }
+    function displayMoreInfo(data) {
+      if (!data) {
+        errorDisplay.textContent = "Weather data is undefined.";
+        return;
+      }
+      const { main, wind, sys } = data;
+      const moreInfoHtml = `
+      <div class="card bg_blue-dark-color p-3 primary-color mt-3" style="width: 18rem;">
+        <div class="card-body">
+          <p class="card-text"><span class="fw-semibold">Humidity:</span> ${main.humidity}%</p>
+          <p class="card-text"><span class="fw-semibold">Wind Speed:</span> ${wind.speed} m/s</p>
+          <p class="card-text"><span class="fw-semibold">Sunrise:</span> ${new Date(
+        sys.sunrise * 1e3
+      ).toLocaleTimeString()}</p>
+          <p class="card-text"><span class="fw-semibold">Sunset:</span> ${new Date(
+        sys.sunset * 1e3
+      ).toLocaleTimeString()}</p>
+        </div>
+      </div>
+      `;
+      const moreInfoContainer = document.createElement("div");
+      moreInfoContainer.innerHTML = moreInfoHtml;
+      weatherDisplay.appendChild(moreInfoContainer);
+    }
+  });
+})();
+
