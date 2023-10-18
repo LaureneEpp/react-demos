063587ca6ee0e37e3cb53bcafd0da59bb6a53505
Laurène Eppellé <Laurene@mbp-de-eppelle.home>
2023-10-18 10:37:48 +0200

    Refactoring links to icons

--- 004-yogaroom-react-rails/frontend/src/features/EditYogaClass.jsx
+++ 004-yogaroom-react-rails/frontend/src/features/EditYogaClass.jsx
@@ -134,6 +134,7 @@ function EditYogaClass() {
                 Date
               </label>
               <input
+                type="date"
                 id="date"
                 className="form-control form-control-lg"
                 name="date"
@@ -142,18 +143,37 @@ function EditYogaClass() {
                 required
               />
             </div>
+            <div className="d-flex">
+              <Link
+                to={`/yoga_classes/${id}`}
+                className="btn btn-lg secondary-color my-3 p-2"
+                role="button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-arrow-left-circle"
+                  viewBox="0 0 16 16">
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                  />
+                </svg>
+              </Link>
 
-            <button type="submit" className="btn my-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="30"
-                height="30"
-                fill="currentColor"
-                class="bi bi-check-lg secondary-color"
-                viewBox="0 0 16 16">
-                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-              </svg>
-            </button>
+              <button type="submit" className="btn my-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  className="bi bi-check-lg secondary-color"
+                  viewBox="0 0 16 16">
+                  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
+                </svg>
+              </button>
+            </div>
           </form>
         </div>
       </div>
