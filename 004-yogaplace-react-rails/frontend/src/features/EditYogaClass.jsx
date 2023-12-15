581305344a0c015dda10ae5b4827923be1cc6b64
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2023-12-15 17:39:38 +0100

    restyling editing forms

--- 004-yogaplace-react-rails/frontend/src/features/EditYogaClass.jsx
+++ 004-yogaplace-react-rails/frontend/src/features/EditYogaClass.jsx
@@ -73,27 +73,18 @@ function EditYogaClass() {
   }
 
   return (
-    <div className="vh-100 m-4">
-      <div className="new-form">
-        <div className="new-form-title col-3">
-          <h2 className="text-uppercase fw-semibold">E</h2>
-          <h2 className="text-uppercase fw-semibold">D</h2>
-          <h2 className="text-uppercase fw-semibold">I</h2>
-          <h2 className="text-uppercase fw-semibold">T</h2>
-          <h2 className="text-uppercase fw-semibold">*</h2>
-          <h2 className="text-uppercase fw-semibold">Y</h2>
-          <h2 className="text-uppercase fw-semibold">O</h2>
-          <h2 className="text-uppercase fw-semibold">G</h2>
-          <h2 className="text-uppercase fw-semibold">A</h2>
-          <h2 className="text-uppercase fw-semibold">*</h2>
-          <h2 className="text-uppercase fw-semibold">C</h2>
-          <h2 className="text-uppercase fw-semibold">L</h2>
-          <h2 className="text-uppercase fw-semibold">A</h2>
-          <h2 className="text-uppercase fw-semibold">S</h2>
-          <h2 className="text-uppercase fw-semibold">S</h2>
-        </div>
-        <div className="col-9 d-flex flex-column align-items-center justify-content-center">
-          <form className=" border border-2 p-4 w-75" onSubmit={handleSubmit}>
+    <div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
+    <div className="container my-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card bg-transparent border border-2 p-2">
+            <div className="card-header p-3 d-flex flex-row">
+              <h2 className="card-title white-color display-4">
+                Edit this class
+              </h2>
+            </div>
+            <div className="card-body">
+            <form className="" onSubmit={handleSubmit}>
             <div className="form-group my-2">
               <label className="my-2" htmlFor="title">
                 Title
@@ -160,10 +151,10 @@ function EditYogaClass() {
                   width="30"
                   height="30"
                   fill="currentColor"
-                  class="bi bi-arrow-left-circle"
+                  className="bi bi-arrow-left-circle"
                   viewBox="0 0 16 16">
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
                   />
                 </svg>
@@ -182,9 +173,13 @@ function EditYogaClass() {
               </button>
             </div>
           </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+  </div>
+
   );
 }
 
--- 004-yogaplace-react-rails/frontend/src/features/EditYogaLesson.jsx
+++ 004-yogaplace-react-rails/frontend/src/features/EditYogaLesson.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import LoadingAnimation from "../components/LoadingAnimation";
 
-
 function EditYogaLesson() {
   const [yoga_lesson, setYogaLesson] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -65,97 +64,89 @@ function EditYogaLesson() {
     }
   };
 
-
   if (!yoga_lesson) {
-    return (
-      <LoadingAnimation/>
-    )
+    return <LoadingAnimation />;
   }
 
   return (
-    <div className="vh-100 m-4">
-      <div className="new-form">
-        <div className="new-form-title col-3">
-          <h2 className="text-uppercase fw-semibold">E</h2>
-          <h2 className="text-uppercase fw-semibold">D</h2>
-          <h2 className="text-uppercase fw-semibold">I</h2>
-          <h2 className="text-uppercase fw-semibold">T</h2>
-          <h2 className="text-uppercase fw-semibold">*</h2>
-          <h2 className="text-uppercase fw-semibold">Y</h2>
-          <h2 className="text-uppercase fw-semibold">O</h2>
-          <h2 className="text-uppercase fw-semibold">G</h2>
-          <h2 className="text-uppercase fw-semibold">A</h2>
-          <h2 className="text-uppercase fw-semibold">*</h2>
-          <h2 className="text-uppercase fw-semibold">L</h2>
-          <h2 className="text-uppercase fw-semibold">E</h2>
-          <h2 className="text-uppercase fw-semibold">S</h2>
-          <h2 className="text-uppercase fw-semibold">S</h2>
-          <h2 className="text-uppercase fw-semibold">O</h2>
-          <h2 className="text-uppercase fw-semibold">N</h2>
-        </div>
-        <div className="col-9 d-flex flex-column align-items-center justify-content-center">
-          <form className=" border border-2 p-4 w-75" onSubmit={handleSubmit}>
-            <div className="form-group my-2">
-              <label className="my-2" htmlFor="title">
-                Title
-              </label>
-              <input
-                type="text"
-                id="title"
-                className="form-control form-control-lg"
-                name="title"
-                value={yoga_lesson.title}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group my-2">
-              <label className="my-2" htmlFor="description">
-                Description
-              </label>
-              <input
-                type="text"
-                id="description"
-                className="form-control form-control-lg"
-                name="description"
-                value={yoga_lesson.description}
-                onChange={handleChange}
-                required
-              />
-            </div>
+    <div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
+      <div className="container my-5">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <div className="card bg-transparent border border-2 p-2">
+              <div className="card-header p-3 d-flex flex-row">
+                <h2 className="card-title white-color display-4">
+                  Edit this lesson
+                </h2>
+              </div>
+              <div className="card-body">
+                <form
+                  className=""
+                  onSubmit={handleSubmit}>
+                  <div className="form-group my-2">
+                    <label className="my-2" htmlFor="title">
+                      Title
+                    </label>
+                    <input
+                      type="text"
+                      id="title"
+                      className="form-control form-control-lg"
+                      name="title"
+                      value={yoga_lesson.title}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-group my-2">
+                    <label className="my-2" htmlFor="description">
+                      Description
+                    </label>
+                    <input
+                      type="text"
+                      id="description"
+                      className="form-control form-control-lg"
+                      name="description"
+                      value={yoga_lesson.description}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
 
-            <div className="d-flex">
-              <Link
-                to={`/yoga_lessons/${id}`}
-                className="btn btn-lg secondary-color my-3 p-2"
-                role="button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  fill="currentColor"
-                  className="bi bi-arrow-left-circle"
-                  viewBox="0 0 16 16">
-                  <path
-                    fillRule="evenodd"
-                    d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
-                  />
-                </svg>
-              </Link>
+                  <div className="d-flex">
+                    <Link
+                      to={`/yoga_lessons/${id}`}
+                      className="btn btn-lg secondary-color my-3 p-2"
+                      role="button">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        fill="currentColor"
+                        className="bi bi-arrow-left-circle"
+                        viewBox="0 0 16 16">
+                        <path
+                          fillRule="evenodd"
+                          d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                        />
+                      </svg>
+                    </Link>
 
-              <button type="submit" className="btn my-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  fill="currentColor"
-                  className="bi bi-check-lg secondary-color"
-                  viewBox="0 0 16 16">
-                  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-                </svg>
-              </button>
+                    <button type="submit" className="btn my-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        fill="currentColor"
+                        className="bi bi-check-lg secondary-color"
+                        viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
+                      </svg>
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
