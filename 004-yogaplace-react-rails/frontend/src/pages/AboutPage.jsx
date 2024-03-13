1423b94da3355322e3838acad5e6b5eb1534d48e
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-03-13 18:22:04 +0100

    refactoring styling aboutpage

--- 004-yogaplace-react-rails/frontend/src/pages/AboutPage.jsx
+++ 004-yogaplace-react-rails/frontend/src/pages/AboutPage.jsx
@@ -28,7 +28,7 @@ function Image({ id }) {
           className="img-fluid rounded mx-auto d-block"
         />
       </motion.div>
-      <div className="m-2 vw-100 d-flex justify-content-center align-items-center">
+      <div className="m-2 w-100 d-flex justify-content-center align-items-center">
         <h6 className="fs-3 border boder-light p-3 fw-light">John Doe</h6>
       </div>
     </div>
@@ -37,28 +37,26 @@ function Image({ id }) {
 
 function AboutPage() {
   return (
-    <div className="vw-100 d-flex align-items-center justify-content-center">
-      <div className="container bg-transparent">
-        <div className="mt-5">
-          <div className="about-section container-fluid mb-5">
-            <h2 className="display-4">About Us</h2>
-            <p className="lead text-muted">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Temporibus pariatur, laudantium dicta repellat iure minus fugit,
-              tenetur facilis, nobis illum voluptatibus deserunt eum enim
-              minima! Aliquam dolorem sed rerum accusamus.
-            </p>
-            <hr className="my-4" />
-          </div>
-          <div className="team-section">
-            <div className="text-center">
-              <h5 className="display-4">Our Team</h5>
-              <div className="d-flex flex-column align-items-center justify-content-center my-4 overflow-scroll">
-                {[1, 2, 3].map((image) => (
-                  <Image key={image} id={image} />
-                ))}
-              </div>
-            </div>
+    <div className="h-100 d-flex flex-column align-items-center justify-content-center">
+      <div className="jumbotron jumbotron-fluid bg-transparent px-4 margin-top-8">
+        <div className="m-3">
+          <h3 className="display-4">About Us</h3>
+          <p className="lead text-muted">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus
+            pariatur, laudantium dicta repellat iure minus fugit, tenetur
+            facilis, nobis illum voluptatibus deserunt eum enim minima! Aliquam
+            dolorem sed rerum accusamus.
+          </p>
+          <hr className="my-4" />
+        </div>
+      </div>
+      <div className="container overflow-auto align-self-center px-5">
+        <div className="text-center">
+          <h5 className="display-4">Our Team</h5>
+          <div className="d-flex flex-column align-items-center justify-content-center my-4 overflow-scroll">
+            {[1, 2, 3].map((image) => (
+              <Image key={image} id={image} />
+            ))}
           </div>
         </div>
       </div>
