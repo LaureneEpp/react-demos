fe8ace3c639a144adb30d8c6f8ddf9337ac52a58
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-03-29 14:42:53 +0100

    refactoring styling title form create

--- 004-yogaplace-react-rails/frontend/src/features/yogaClass/YogaClassCreate.jsx
+++ 004-yogaplace-react-rails/frontend/src/features/yogaClass/YogaClassCreate.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import BackButton from "../../components/Button/BackButton";
-import SubmitButton from "../../components/Button/SubmitButton"
+import SubmitButton from "../../components/Button/SubmitButton";
 import useFetchYogaLessonData from "../../services/useFetchYogaLessonData";
 
 function YogaClassCreate({ currUser }) {
@@ -18,8 +18,9 @@ function YogaClassCreate({ currUser }) {
       user_id: "",
     },
   });
-  const { yogaLessonsList, setYogaClassData, API_URL } = useFetchYogaLessonData();
+  const { yogaLessonsList, setYogaClassData, API_URL } =
     useFetchYogaLessonData();
+  useFetchYogaLessonData();
   const navigate = useNavigate();
   const [, setError] = useState(null);
 
@@ -46,69 +47,57 @@ function YogaClassCreate({ currUser }) {
     } catch (error) {
       setError(
         `An error occurred while fetching lesson data: ${error.message}`
-        );
-      }
-      navigate(`/yoga_classes`);
+      );
+    }
+    navigate(`/yoga_classes`);
   };
 
   return (
-    <div className="vh-100 m-4">
-      <div className="new-form">
-        {/* <div className="new-form-title col-3">
-          <h2 className="text-uppercase fw-semibold">N</h2>
-          <h2 className="text-uppercase fw-semibold">E</h2>
-          <h2 className="text-uppercase fw-semibold">W</h2>
-          <h2 className="text-uppercase fw-semibold">*</h2>
-          <h2 className="text-uppercase fw-semibold">Y</h2>
-          <h2 className="text-uppercase fw-semibold">O</h2>
-          <h2 className="text-uppercase fw-semibold">G</h2>
-          <h2 className="text-uppercase fw-semibold">A</h2>
-          <h2 className="text-uppercase fw-semibold">*</h2>
-          <h2 className="text-uppercase fw-semibold">C</h2>
-          <h2 className="text-uppercase fw-semibold">L</h2>
-          <h2 className="text-uppercase fw-semibold">A</h2>
-          <h2 className="text-uppercase fw-semibold">S</h2>
-          <h2 className="text-uppercase fw-semibold">S</h2>
-        </div> */}
-        <div className="col-9 d-flex flex-column align-items-center justify-content-center">
-          <form className=" border border-2 p-4"onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-group my-2">
-            <select
-                {...register("yoga_lesson_id", { required: true })}
-                className="form-control form-control-lg">
-                <option value="">Select a lesson</option>
-                {yogaLessonsList.map((lesson) => (
-                  <option key={lesson.id} value={lesson.id}>
-                    {lesson.title}
-                  </option>
-                ))}
-              </select>
-              {errors.yoga_lesson_id && <p>This field is required!</p>}
-            </div>
-            <div className="form-group my-2">
-            <input
-                {...register("location", { required: true })}
-                placeholder="Location"
-                className="form-control form-control-lg"
-              />
-              {errors.location && <p>This field is required!</p>}
-            </div>
-            <div className="form-group my-2">
-            <input
-                {...register("date", { required: true })}
-                type="date"
-                placeholder="Date"
-                className="form-control form-control-lg"
-              />
-              {errors.date && <p>This field is required!</p>}
-
-            </div>
-            <div className="d-flex">
-              <BackButton path={"/yoga_classes"} />
-              <SubmitButton/>
-            </div>
-          </form>
+    <div className="h-100 m-4">
+      <div className="h-100 d-flex flex-column align-items-center justify-content-center">
+        <div className="text-center px-4 margin-top-8">
+          <h3 className="display-4 text-uppercase fw-semibold">
+            Add a new class
+          </h3>
         </div>
+        <form
+          className=" border border-2 p-4 my-5"
+          onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group my-2">
+            <select
+              {...register("yoga_lesson_id", { required: true })}
+              className="form-control form-control-lg">
+              <option value="">Select a lesson</option>
+              {yogaLessonsList.map((lesson) => (
+                <option key={lesson.id} value={lesson.id}>
+                  {lesson.title}
+                </option>
+              ))}
+            </select>
+            {errors.yoga_lesson_id && <p>This field is required!</p>}
+          </div>
+          <div className="form-group my-2">
+            <input
+              {...register("location", { required: true })}
+              placeholder="Location"
+              className="form-control form-control-lg"
+            />
+            {errors.location && <p>This field is required!</p>}
+          </div>
+          <div className="form-group my-2">
+            <input
+              {...register("date", { required: true })}
+              type="date"
+              placeholder="Date"
+              className="form-control form-control-lg"
+            />
+            {errors.date && <p>This field is required!</p>}
+          </div>
+          <div className="d-flex">
+            <BackButton path={"/yoga_classes"} />
+            <SubmitButton />
+          </div>
+        </form>
       </div>
     </div>
   );
--- 004-yogaplace-react-rails/frontend/src/features/yogaLesson/YogaLessonCreate.jsx
+++ 004-yogaplace-react-rails/frontend/src/features/yogaLesson/YogaLessonCreate.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import BackButton from "../../components/Button/BackButton";
-import SubmitButton from "../../components/Button/SubmitButton"
+import SubmitButton from "../../components/Button/SubmitButton";
 import useFetchYogaLessonData from "../../services/useFetchYogaLessonData";
 
 function YogaLessonCreate() {
@@ -26,7 +26,10 @@ function YogaLessonCreate() {
     const formData = new FormData();
     formData.append("yoga_lesson[title]", yogaLessonData.title);
     formData.append("yoga_lesson[description]", yogaLessonData.description);
-    formData.append("yoga_lesson[yoga_category_id]",yogaLessonData.yoga_category_id);
+    formData.append(
+      "yoga_lesson[yoga_category_id]",
+      yogaLessonData.yoga_category_id
+    );
 
     try {
       const yogaLessonsResponse = await fetch(`${API_URL}/yoga_lessons`, {
@@ -51,47 +54,48 @@ function YogaLessonCreate() {
   };
 
   return (
-    <div className="vh-100 m-4">
-      <div className="new-form">
-        <div className="col-9 d-flex flex-column align-items-center justify-content-center">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className=" border border-2 p-4">
-            <div className="form-group my-2">
-              <select
-                {...register("yoga_category_id", { required: true })}
-                className="form-control form-control-lg">
-                <option value="">Select a category</option>
-                {yogaCategoriesList.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.title}
-                  </option>
-                ))}
-              </select>
-              {errors.yoga_category_id && <p>This field is required!</p>}
-            </div>
-            <div className="form-group my-2">
-              <input
-                {...register("title", { required: true })}
-                placeholder="Title"
-                className="form-control form-control-lg"
-              />
-              {errors.title && <p>This field is required!</p>}
-            </div>
-            <div className="form-group my-2">
-              <input
-                {...register("description", { required: true })}
-                placeholder="Description"
-                className="form-control form-control-lg"
-              />
-              {errors.description && <p>This field is required!</p>}
-            </div>
-            <div className="d-flex">
-              <BackButton path={"/yoga_lessons"} />
-              <SubmitButton/>
-            </div>
-          </form>
+    <div className="h-100 m-4">
+      <div className="h-100 d-flex flex-column align-items-center justify-content-center">
+        <div className="text-center px-4 margin-top-8">
+          <h3 className="display-4 text-uppercase fw-semibold">Add a new lesson</h3>
         </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className=" border border-2 p-4 my-5">
+          <div className="form-group my-2">
+            <select
+              {...register("yoga_category_id", { required: true })}
+              className="form-control form-control-lg">
+              <option value="">Select a category</option>
+              {yogaCategoriesList.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.title}
+                </option>
+              ))}
+            </select>
+            {errors.yoga_category_id && <p>This field is required!</p>}
+          </div>
+          <div className="form-group my-2">
+            <input
+              {...register("title", { required: true })}
+              placeholder="Title"
+              className="form-control form-control-lg"
+            />
+            {errors.title && <p>This field is required!</p>}
+          </div>
+          <div className="form-group my-2">
+            <input
+              {...register("description", { required: true })}
+              placeholder="Description"
+              className="form-control form-control-lg"
+            />
+            {errors.description && <p>This field is required!</p>}
+          </div>
+          <div className="d-flex">
+            <BackButton path={"/yoga_lessons"} />
+            <SubmitButton />
+          </div>
+        </form>
       </div>
     </div>
   );
