fd11f687a90eabac3478282d0a83f81ba58875b9
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-05-29 17:18:55 +0200

    customise profile page

--- 005-redux-demo/src/features/User/UserProfile.jsx
+++ 005-redux-demo/src/features/User/UserProfile.jsx
@@ -1,26 +1,26 @@
 const UserProfile = ({ authUser }) => {
   return (
-    <div className="overflow-hidden h-full bg-neutral-100 py-24 sm:py-32">
+    <div className="overflow-hidden h-full bg-neutral-100 py-24 sm:py-32 flex items-center">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8 lg:pt-4">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-purple-600">
-                Your profile
-              </h2>
-              <div key={authUser.id} className="mt-8">
+        <div className="lg:max-w-lg bg-emerald-800 p-4 rounded shadow-xl">
+          <div className="p-3">
+            <h2 className="text-2xl font-semibold leading-7 text-purple-600">
+              Your profile
+            </h2>
+            <div key={authUser.id} className="mt-8">
+              <div className="flex flex-row items-center justify-around p-4">
                 <img
                   src={authUser.img}
                   alt="avatar"
-                  className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center"
+                  className="relative inline-block h-28 w-28 rounded-full  object-cover object-center"
                 />
-                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                <p className="mt-2 text-6xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                   {authUser.fullname}
                 </p>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                  {authUser.bio}
-                </p>
               </div>
+              <p className="mt-6 text-lg leading-8 text-neutral-100 text-justify">
+                {authUser.bio}
+              </p>
             </div>
           </div>
         </div>
