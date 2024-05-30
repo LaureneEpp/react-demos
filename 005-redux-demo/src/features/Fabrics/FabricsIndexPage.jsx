2fd6fa61293245ed61a028055d57ba35db2f98ae
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-05-30 20:07:34 +0200

    fixing index page'

--- 005-redux-demo/src/features/Fabrics/FabricsIndexPage.jsx
+++ 005-redux-demo/src/features/Fabrics/FabricsIndexPage.jsx
@@ -1,11 +1,29 @@
-import FabricsIndexSection from "./FabricsIndexSection"
+import FabricsIndexItem from "./FabricsIndexItem";
+import { useSelector } from "react-redux";
 
 const FabricsIndexPage = () => {
-  return (
-      <div>
-       <FabricsIndexSection></FabricsIndexSection>   
-    </div>
-  )
-}
+  const fabrics = useSelector((state) => state.fabrics.fabricsIndex);
 
-export default FabricsIndexPage
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center items-center py-5 m-5">
+        {fabrics.map((fabric, index) => {
+          return (
+            <div key={fabric.id}>
+              <FabricsIndexItem
+                id={fabric.id}
+                name={fabric.name}
+                img={fabric.img}
+                text={fabric.text}
+                color={fabric.color}
+                price={fabric.price}
+                type={fabric.type}></FabricsIndexItem>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FabricsIndexPage;
