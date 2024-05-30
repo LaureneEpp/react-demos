7a395cd87988c71b7fb654cec68afdf893a1ab84
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-05-30 16:43:31 +0200

    fixing redirecting on mainpage

--- 005-redux-demo/src/features/Fabrics/FabricsIndexItem.jsx
+++ 005-redux-demo/src/features/Fabrics/FabricsIndexItem.jsx
@@ -1,11 +1,15 @@
 import { useDispatch } from "react-redux";
 import { addToCart } from "../Cart/cartSlice";
 import { Link } from "react-router-dom";
+import { fabricShowPage } from "./fabricsSlice";
 
 const FabricsIndexItem = ({ id, img, name, text, price, color, type }) => {
-  
   const dispatch = useDispatch();
   const defaultColor = "black";
+
+  const handleCardClick = () => {
+    dispatch(fabricShowPage(id));
+  };
 
   const handleAddToCart = () => {
     dispatch(
@@ -24,19 +28,22 @@ const FabricsIndexItem = ({ id, img, name, text, price, color, type }) => {
     <div>
       <div className="relative flex flex-col m-6 bg-neutral-100 shadow-md bg-clip-border rounded-xl w-96">
         <Link to={`/fabrics/${type}/${id}`}>
-          <div className=" h-48 overflow-hidden text-neutral-100 shadow-lg bg-clip-border rounded-t-xl bg-emerald-500 shadow-blue-gray-500/40">
-            <img src={img} alt="name" className="object-cover" />
-          </div>
-          <div className="p-6">
-            <h5 className="block mb-2 font-inter text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-              {name}
-            </h5>
-            <p className="block font-inter text-base antialiased font-light leading-relaxed text-inherit text-justify">
-              {text}
-            </p>
-            <p className="block font-inter text-base antialiased font-light leading-relaxed text-inherit">
-              {defaultColor}
-            </p>
+          <div
+            onClick={handleCardClick}>
+            <div className=" h-48 overflow-hidden text-neutral-100 shadow-lg bg-clip-border rounded-t-xl bg-emerald-500 shadow-blue-gray-500/40">
+              <img src={img} alt="name" className="object-cover" />
+            </div>
+            <div className="p-6">
+              <h5 className="block mb-2 font-inter text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                {name}
+              </h5>
+              <p className="block font-inter text-base antialiased font-light leading-relaxed text-inherit text-justify">
+                {text}
+              </p>
+              <p className="block font-inter text-base antialiased font-light leading-relaxed text-inherit">
+                {defaultColor}
+              </p>
+            </div>
           </div>
         </Link>
         <div className="p-6 pt-0">
